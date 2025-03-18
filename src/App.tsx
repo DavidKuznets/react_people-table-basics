e444606def540903c212fc020193fcf7df1e83b4
DavidKuznets <davidkuznets7@gmail.com>
2025-03-18 13:12:51 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PageNotFound from './page/PageNotFound';
 import PeoplePage from './page/PeoplePage';
 import PersonDetailsPage from './page/PersonDetailsPage';
 import { Navigations } from './components/Loader/Navigations';
+import { Navigate } from 'react-router-dom';
 
 export const App = () => {
   const [loading, setLoading] = useState(true);
@@ -17,24 +18,32 @@ export const App = () => {
     }, 3000);
   }, []);
 
-  if (loading) {
-    return <Loader />;
+  if (window.location.pathname === '/home') {
+    return <Navigate to="/" replace />;
   }
 
   return (
     <div data-cy="app">
+      {loading ? (
+        <>
+          <main className="section">
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/people" element={<PeoplePage />} />
+                <Route
+                  path="/people/:slug"
+                  element={<PersonDetailsPage />}
+                />{' '}
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </div>
+          </main>
+        </>
+      ) : (
+        <Loader />
+      )}
       <Navigations />
-
-      <main className="section">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/people" element={<PeoplePage />} />
-            <Route path="/people/:slug" element={<PersonDetailsPage />} />{' '}
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </div>
-      </main>
     </div>
   );
 };
